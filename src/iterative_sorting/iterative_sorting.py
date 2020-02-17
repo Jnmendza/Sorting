8e32dd054e617bcb96742845f665b8fddca889ae
Jnmendza <jonatmendoza@gmail.com>
2020-02-17 12:13:47 -0800

    Initial Commit

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,4 +1,5 @@
-# TO-DO: Complete the selection_sort() function below 
+# Initial Commit
+# TO-DO: Complete the selection_sort() function below
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
